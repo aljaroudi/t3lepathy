3bf407d4076dc5bb0257dd2aedaaa66cde42a40f
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-06-11 15:37:54 +0300

    Implement chat interface with message input and chat selection

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,67 @@
-<main>
-  <h1>T3lepathy</h1>
+<script lang="ts">
+  import {
+    addMessage,
+    createChat,
+    state as db,
+    getChats,
+    setCurrentChat,
+  } from "./lib/db.svelte"
+
+  getChats()
+</script>
+
+<main class="grid h-dvh w-full" style="grid-template-columns: 200px 1fr">
+  <aside class="border-r border-gray-200 bg-gray-100 p-4">
+    <h1 class="text-2xl font-bold">T3lepathy</h1>
+    <div class="flex flex-col gap-2">
+      <button
+        class="rounded-xl border p-2 bg-blue-500 text-white hover:bg-blue-600 cursor-pointer"
+        onclick={() => createChat("New chat")}
+      >
+        +
+      </button>
+      {#each db.chats as chat}
+        <button
+          class="flex rounded-xl border-2 p-2 aria-pressed:border-blue-500"
+          aria-pressed={db.currentChatId === chat.id}
+          onclick={() => setCurrentChat(chat.id)}
+        >
+          {chat.title}
+        </button>
+      {/each}
+    </div>
+  </aside>
+  <section class="flex flex-col gap-2 p-2">
+    {#each db.messages as message}
+      <div class="rounded-xl border p-2">
+        <p>{message.content}</p>
+      </div>
+    {/each}
+    <form
+      class="mt-auto flex gap-2"
+      onsubmit={(e) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const message = formData.get("message") as string
+        if (!db.currentChatId) return alert("No chat selected")
+        addMessage({
+          id: crypto.randomUUID(),
+          chatId: db.currentChatId,
+          content: message.trim(),
+          role: "user",
+          date: new Date(),
+        })
+      }}
+    >
+      <input
+        name="message"
+        type="text"
+        class="flex-1 rounded-xl border p-2"
+        placeholder="Type your message..."
+        minLength={1}
+        required
+      />
+      <button type="submit" class="rounded-xl border p-2"> Send </button>
+    </form>
+  </section>
 </main>
