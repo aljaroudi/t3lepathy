e06ad2fb93056c80b689dc5af0d2dc09d1bb5cc7
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-06-14 19:06:30 +0300

    feat: implement dialog for API key management and enhance model selection with provider validation

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,33 @@
 <script lang="ts">
   import { marked } from "marked"
-  import { MODELS } from "./lib/ai"
+  import { MODELS, PROVIDERS } from "./lib/ai"
   import {
     addMessage,
     createChat,
     state as db,
-    getChats,
+    deleteApiKey,
+    initDB,
+    setApiKey,
     setCurrentChat,
   } from "./lib/db.svelte"
-  import type { Model } from "./lib/types"
+  import type { Model, Provider } from "./lib/types"
+  import Trash from "./lib/icons/Trash.svelte"
+  import Plus from "./lib/icons/Plus.svelte"
 
-  getChats()
+  initDB()
 
   // TODO: allow user to select model
   let currentModel = $state<Model["name"]>("gemini-1.5-flash")
-  // TODO: allow user to input api keys for each provider
-  const apiKey = ""
+
+  let showDialog = $state(false)
 
   async function sendMessage(message: string) {
     if (!db.currentChatId) return alert("No chat selected")
 
     const model = MODELS.find((m) => m.name === currentModel)
     if (!model) return alert("Invalid model")
+    const apiKey = db.apiKeys[model.provider]
+    if (!apiKey) return alert("No API key found for this model")
     // User
     addMessage(
       {
@@ -40,7 +46,7 @@
 <main class="grid h-dvh w-full" style="grid-template-columns: 200px 1fr">
   <aside class="border-r border-gray-200 bg-gray-100 p-4">
     <h1 class="text-2xl font-bold">T3lepathy</h1>
-    <div class="flex flex-col gap-2">
+    <div class="flex flex-col gap-2 h-full">
       <button
         class="rounded-xl border p-2 bg-blue-500 text-white hover:bg-blue-600 cursor-pointer"
         onclick={() => createChat("New chat")}
@@ -56,6 +62,12 @@
           {chat.title}
         </button>
       {/each}
+      <button
+        class="rounded-xl border p-2 bg-gray-300 hover:bg-gray-400 cursor-pointer mt-auto"
+        onclick={() => (showDialog = true)}
+      >
+        Open Dialog
+      </button>
     </div>
   </aside>
   <section class="flex flex-col gap-2 px-4 py-2">
@@ -78,8 +90,15 @@
       }}
     >
       <select name="model" id="model" required bind:value={currentModel}>
-        {#each MODELS as model}
-          <option value={model.name}>{model.title}</option>
+        {#each PROVIDERS as provider}
+          {@const keyIsSet = db.apiKeys[provider]?.length}
+          <optgroup label={keyIsSet ? provider : `${provider} (no key)`}>
+            {#each MODELS.filter((model) => model.provider === provider) as model}
+              <option value={model.name} disabled={!keyIsSet}>
+                {model.title}
+              </option>
+            {/each}
+          </optgroup>
         {/each}
       </select>
       <input
@@ -94,3 +113,86 @@
     </form>
   </section>
 </main>
+
+{#if showDialog}
+  <div
+    class="fixed inset-0 bg-black/30 flex items-center justify-center z-50 backdrop-blur-xs"
+  >
+    <div class="bg-white rounded-xl shadow-lg p-6 min-w-[300px] relative">
+      <button
+        class="absolute top-2 right-2 text-gray-500 hover:text-black"
+        onclick={() => (showDialog = false)}
+        >&times;
+      </button>
+      <h2 class="text-xl font-bold mb-2">Settings</h2>
+      <p>This is a simple dialog popup. You can put any content here.</p>
+      <form
+        class="grid gap-2 bg-gray-100 p-2 rounded-xl border border-gray-200 shadow-xs items-center"
+        style="grid-template-columns: 20ch 2fr 1fr"
+        onsubmit={(e) => {
+          e.preventDefault()
+          const formData = new FormData(e.currentTarget)
+          const provider = formData.get("provider") as Provider
+          const apiKey = formData.get("apiKey") as string
+          if (!apiKey) return alert("Please enter an API key")
+          if (!PROVIDERS.includes(provider)) return alert("Invalid provider")
+          setApiKey(provider, apiKey)
+        }}
+      >
+        <!-- provider selection -->
+        <select
+          name="provider"
+          id="provider"
+          required
+          class="rounded-xl border p-2 bg-white border-gray-200 overflow-hidden w-fit"
+        >
+          {#each PROVIDERS as provider}
+            <option value={provider}>{provider}</option>
+          {/each}
+        </select>
+
+        <!-- api key input -->
+        <input
+          type="text"
+          name="apiKey"
+          id="apiKey"
+          required
+          class="flex-1 rounded bg-white"
+          placeholder="Enter your API key"
+        />
+
+        <!-- save button -->
+        <button
+          type="submit"
+          class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-fit ml-auto"
+        >
+          <Plus />
+        </button>
+      </form>
+      <!-- saved keys -->
+      <div class="flex flex-col gap-2 my-4">
+        {#if Object.keys(db.apiKeys).length > 0}
+          {#each Object.entries(db.apiKeys) as [provider, key]}
+            <div
+              class="grid gap-2 items-center p-2 rounded-xl border border-gray-200 bg-gray-50"
+              style="grid-template-columns: 20ch 2fr 1fr"
+            >
+              <label for={`apiKey-${provider}`}>{provider}</label>
+              <p class="text-gray-500 text-sm">{key.slice(0, 20)}</p>
+              <button
+                class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 cursor-pointer w-fit ml-auto"
+                onclick={() => deleteApiKey(provider as Provider)}
+              >
+                <Trash />
+              </button>
+            </div>
+          {/each}
+        {:else}
+          <p class="p-4 text-gray-500 text-center">
+            No API keys saved. Please enter your API keys below.
+          </p>
+        {/if}
+      </div>
+    </div>
+  </div>
+{/if}
