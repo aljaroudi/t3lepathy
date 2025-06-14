45ca95210b23e2bf9b270c0da4ea8b132829af20
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-06-14 20:41:31 +0300

    fix: remove redundant class from Trash icon for cleaner markup

--- src/lib/icons/Trash.svelte
+++ src/lib/icons/Trash.svelte
@@ -8,7 +8,6 @@
   stroke-width="2"
   stroke-linecap="round"
   stroke-linejoin="round"
-  class="lucide lucide-trash2-icon lucide-trash-2"
 >
   <path d="M3 6h18" />
   <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
