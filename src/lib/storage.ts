a9aa75e46d3e817cf6d9958fe712274cf8318f39
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-07-03 21:55:25 +0300

    refactor: rename convertImageToBase64 to convertFileToBase64 and extend functionality to handle both image and file types

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,16 +1,25 @@
-import type { ImagePart } from 'ai'
+import type { FilePart, ImagePart } from 'ai'
 
 /**
  * Convert an image to a base64 string
- * @param image - The image to convert
+ * @param file - The image to convert
  * @returns A promise that resolves to the base64 string
  */
-export async function convertImageToBase64(image: File): Promise<ImagePart> {
+export async function convertFileToBase64(
+	file: File
+): Promise<ImagePart | FilePart> {
 	return new Promise((resolve, reject) => {
 		const reader = new FileReader()
 		reader.onload = () =>
-			resolve({ type: 'image', image: reader.result as string })
+			file.type.startsWith('image/')
+				? resolve({ type: 'image', image: reader.result as string })
+				: resolve({
+						type: 'file',
+						data: reader.result as string,
+						mimeType: file.type,
+						filename: file.name,
+					})
 		reader.onerror = reject
-		reader.readAsDataURL(image)
+		reader.readAsDataURL(file)
 	})
 }
