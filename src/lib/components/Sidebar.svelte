1b866cbac67fe17e4c04e346b514f93e3d76b1bc
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-07-17 15:20:14 +0300

    style: adjust Sidebar component to set a maximum height for improved layout and responsiveness

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -32,7 +32,7 @@
 </script>
 
 <aside
-	class="flex flex-col gap-2 bg-gradient-to-b from-slate-100 to-slate-200 p-4 dark:bg-slate-800 dark:from-cyan-950 dark:to-slate-950"
+	class="flex max-h-dvh flex-col gap-2 bg-gradient-to-b from-slate-100 to-slate-200 p-4 dark:bg-slate-800 dark:from-cyan-950 dark:to-slate-950"
 >
 	<div class="my-1 flex items-center justify-between gap-2">
 		<button
