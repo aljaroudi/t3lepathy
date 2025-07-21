48627e955c5f1f36814c5dd388f38da9013d8956
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-07-21 16:18:43 +0300

    style: enhance hover effects on ActionButton component for improved user interaction

--- src/lib/components/ActionButton.svelte
+++ src/lib/components/ActionButton.svelte
@@ -6,7 +6,7 @@
 
 <button
 	{...rest}
-	class="ml-auto flex aspect-square cursor-pointer items-center justify-center rounded-full bg-radial-[at_25%_25%] from-cyan-500 to-cyan-700 to-75% p-2 text-lg text-white shadow-lg shadow-cyan-500/20 hover:bg-radial-[at_50%_50%] disabled:cursor-not-allowed disabled:opacity-50"
+	class="ml-auto flex aspect-square cursor-pointer items-center justify-center rounded-full bg-radial-[at_25%_25%] from-cyan-500 to-cyan-700 to-75% p-2 text-lg text-white shadow-lg shadow-cyan-500/20 hover:bg-radial-[at_50%_50%] hover:from-cyan-400 hover:to-cyan-500 hover:shadow-cyan-500/50 disabled:cursor-not-allowed disabled:opacity-50"
 >
 	{@render children?.()}
 </button>
