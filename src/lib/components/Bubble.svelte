eeff95c927e2d7214f2a8b956a3cb0df627ae4ba
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-07-25 13:06:50 +0300

    feat: enhance Bubble component with clipboard copy functionality, display token count, and pricing information for messages

--- src/lib/components/Bubble.svelte
+++ src/lib/components/Bubble.svelte
@@ -3,21 +3,29 @@
 	import type { Message } from '../types'
 	import { marked } from './markdown'
 	import { cn } from '../utils'
+	import { getPrice } from '../ai'
+	import { ClipboardCopyIcon } from '@lucide/svelte'
 
 	let { message, loading }: { message: Message; loading: boolean } = $props()
 
-	const attachments = message.content.filter(part => part.type !== 'text')
+	const attachments = $derived(
+		message.content.filter(part => part.type !== 'text')
+	)
 	const maxWidth = message.role === 'user' ? '60ch' : '80ch'
+
+	const textParts = $derived(
+		message.content.filter(part => part.type === 'text') as TextPart[]
+	)
 </script>
 
-<div
+<section
 	class={message.role === 'user'
 		? 'my-2 ml-auto rounded-2xl rounded-br-none bg-zinc-100 px-4 py-2 text-slate-800 shadow-xs aria-busy:animate-pulse'
 		: ''}
 	style="max-width: min(var(--content-width), {maxWidth})"
 	aria-busy={loading}
 >
-	{#each message.content.filter(part => part.type === 'text') as TextPart[] as part}
+	{#each textParts as part}
 		{@html marked.parse(part.text)}
 	{/each}
 	{#if attachments.length > 0}
@@ -40,4 +48,30 @@
 			{/each}
 		</div>
 	{/if}
-</div>
+	<div class="flex items-center gap-2 p-2">
+		<!-- copy to clipboard button -->
+		<button
+			class="cursor-pointer text-xs text-slate-500 hover:text-slate-700"
+			onclick={() =>
+				navigator.clipboard.writeText(textParts.map(p => p.text).join('\n'))}
+			title="Copy to clipboard"
+		>
+			<ClipboardCopyIcon size="1.25em" />
+		</button>
+		{#if message.tokens}
+			<p class="text-xs text-slate-500">
+				{message.tokens.toLocaleString(undefined)} tokens
+			</p>
+		{/if}
+		{#if getPrice(message)}
+			<p class="text-xs text-slate-500">
+				{getPrice(message)?.toLocaleString(undefined, {
+					style: 'currency',
+					currency: 'USD',
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 10,
+				})}
+			</p>
+		{/if}
+	</div>
+</section>
