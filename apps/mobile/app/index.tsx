68bcfbedf0de9f2be796796626986de19b99a2e3
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-07-26 15:09:32 +0300

    feat: initialize mobile app with Expo, including configuration, dependencies, and basic layout

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+export default function Index() {
+	return (
+		<View className="flex-1 justify-center items-center">
+			<Text className="text-rose-800 font-bold text-2xl">
+				Edit app/index.tsx
+			</Text>
+		</View>
+	)
+}
---
+++ apps/mobile/prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+export default {
+  useTabs: true,
+  trailingComma: "es5",
+  singleQuote: true,
+  semi: false,
+  arrowParens: "avoid",
+}
