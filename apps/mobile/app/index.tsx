de5dba2e3e1421789d29d291c28040afacb5e843
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-07-27 11:46:40 +0300

    fix: reorder class names in Text component for consistency in index.tsx

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 export default function Index() {
 	return (
 		<View className="flex-1 justify-center items-center">
-			<Text className="text-rose-800 font-bold text-2xl">
+			<Text className="text-2xl font-bold text-rose-800">
 				Edit app/index.tsx
 			</Text>
 		</View>
