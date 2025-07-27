0164c0af65b74382166382c9784382fb28bd6ff0
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-07-27 15:18:14 +0300

    feat: add counter functionality and improve layout in mobile app index screen

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,11 +1,27 @@
-import { Text, View } from 'react-native'
+import { useState } from 'react'
+import { View } from 'react-native'
+import { Stack } from 'expo-router'
+import { Button } from '@/components/ui/button'
+
+import { Text } from '~/components/ui/text'
 
 export default function Index() {
+	const [count, setCount] = useState(0)
+
 	return (
-		<View className="flex-1 items-center justify-center">
-			<Text className="text-2xl font-bold text-rose-800">
-				Edit app/index.tsx
-			</Text>
-		</View>
+		<>
+			<Stack.Screen options={{ title: 'Home' }} />
+			<View className="flex-1 items-center justify-center gap-2 dark:bg-cyan-950 dark:text-cyan-200">
+				<Text className="text-2xl font-bold dark:text-cyan-50">
+					Edit app/index.tsx
+				</Text>
+				<Button
+					className="dark:bg-cyan-50"
+					onPress={() => setCount(prev => prev + 1)}
+				>
+					<Text className="dark:text-cyan-950">Count: {count}</Text>
+				</Button>
+			</View>
+		</>
 	)
 }
