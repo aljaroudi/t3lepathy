f3f2125c265a77657dee4a9d868d2e03874f7feb
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-07-27 15:16:32 +0300

    feat: integrate ThemeProvider and loading state in RootLayout for improved theming and performance

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,7 +1,30 @@
 import { Stack } from 'expo-router'
 
+import { ThemeProvider } from '@react-navigation/native'
+import { useLayoutEffect, useRef, useState } from 'react'
+import { THEME } from '~/lib/constants'
+import { useColorScheme } from '~/lib/useColorScheme'
 import '../global.css'
 
+
 export default function RootLayout() {
-	return <Stack />
+	const hasMounted = useRef(false)
+	const [isLoaded, setIsLoaded] = useState(false)
+	const { colorScheme } = useColorScheme()
+
+	useLayoutEffect(() => {
+		if (hasMounted.current) return
+		setIsLoaded(true)
+		hasMounted.current = true
+	}, [])
+
+	if (!isLoaded) return null
+
+	return (
+		<ThemeProvider value={THEME[colorScheme]}>
+			<Stack />
+		</ThemeProvider>
+	)
 }
+
+export { ErrorBoundary } from 'expo-router'
